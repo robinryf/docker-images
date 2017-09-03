d90a1c369bcd62826756bf645fdb42110b5c8e2f
Robin Fischer <t9RCtCxQZFp@gmail.com>
2017-09-03 12:53:20 +0200

    Add url for unity3d 5.6.3

--- unity3d/get-unity.sh
+++ unity3d/get-unity.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-URL=
+URL=http://beta.unity3d.com/download/d72e16ff4aba/unity-editor_amd64-5.6.3xf1Linux.deb
 
 echo "Start Unity3D installer download..."
 curl -o /app/unity_editor.deb -s "${URL}" 
