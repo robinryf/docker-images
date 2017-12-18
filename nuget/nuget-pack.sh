ee700a8e17c6030d1eab8df589e395259ee6fea2
Robin Fischer <t9RCtCxQZFp@gmail.com>
2017-12-18 23:32:14 +0100

    Use exe directly

--- nuget/nuget-pack.sh
+++ nuget/nuget-pack.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-mono ${DIR}/nuget.sh pack "$@" -MsbuildPath /usr/lib/mono/msbuild/15.0/bin/
+mono ${DIR}/nuget.exe pack "$@" -MsbuildPath /usr/lib/mono/msbuild/15.0/bin/
