867b0e31816f6fc5371ec9bfc0613bf4488d8c2c
Robin Fischer <t9RCtCxQZFp@gmail.com>
2018-01-06 00:43:39 +0100

    Set version specific download url

--- unity3d/get-unity.sh
+++ unity3d/get-unity.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-URL=
+URL=http://beta.unity3d.com/download/f4fc8fd4067d/unity-editor_amd64-2017.1.1xf1Linux.deb
 
 echo "Start Unity3D installer download..."
-curl -o /app/unity_editor.deb -s "${URL}" 
+curl -o /app/unity_editor.deb -s "${URL}"
 echo "Unity3D installer has finished downloading."
