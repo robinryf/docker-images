438dbed142245ce3cdd7e6ee6826f6977265b62c
Robin Fischer <t9RCtCxQZFp@gmail.com>
2018-01-06 10:14:41 +0100

    Change installer script for new unity linux installer

--- unity3d/get-unity.sh
+++ unity3d/get-unity.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
 URL=http://beta.unity3d.com/download/3c89f8d277f5/UnitySetup-2017.3.0f1
+INSTALL_LOCATION=/opt/Unity
+DOWNLOAD_LOCATION=/app/unity_download
+UNITY_COMPONENTS="Unity"
 
 echo "Start Unity3D installer download..."
 curl -o /app/unity_setup -s "${URL}"
 chmod +x /app/unity_setup
-echo "Unpacking deb package."
-mkdir /opt/Unity
-/app/unity_setup --unattended --components="Unity" --install-location /opt/Unity/
+echo "Start Unity3D installer."
+mkdir ${INSTALL_LOCATION}
+mkdir ${DOWNLOAD_LOCATION}
+printf 'y\n' | /app/unity_setup --unattended --components=${UNITY_COMPONENTS} --install-location ${INSTALL_LOCATION} --download-location ${DOWNLOAD_LOCATION}
+rm -rf ${DOWNLOAD_LOCATION}
