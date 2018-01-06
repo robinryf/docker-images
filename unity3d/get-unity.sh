ab8397105c8690b5dcc59027d0ac9850f238844a
Robin Fischer <t9RCtCxQZFp@gmail.com>
2018-01-06 01:55:14 +0100

    Fix setup script for 2017.3

--- unity3d/get-unity.sh
+++ unity3d/get-unity.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-URL=http://beta.unity3d.com/download/ee86734cf592/unity-editor_amd64-2017.2.0f3.deb
+URL=http://beta.unity3d.com/download/3c89f8d277f5/UnitySetup-2017.3.0f1
 
 echo "Start Unity3D installer download..."
-curl -o /app/unity_editor.deb -s "${URL}"
+curl -o /app/unity_setup -s "${URL}"
+chmod +x /app/unity_setup
 echo "Unpacking deb package."
-dpkg -i /app/unity_editor.deb
-echo "Remove deb package"
-rm /app/unity_editor.deb
+mkdir /opt/Unity
+/app/unity_setup --unattended --components="Unity" --install-location /opt/Unity/
