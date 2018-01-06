0093b7e6b4e0c0c3c4d83ddf802e69cc83ac2285
Robin Fischer <t9RCtCxQZFp@gmail.com>
2018-01-06 01:42:50 +0100

    Move unpack and remove step to bash script

--- unity3d/get-unity.sh
+++ unity3d/get-unity.sh
@@ -3,5 +3,8 @@
 URL=
 
 echo "Start Unity3D installer download..."
-curl -o /app/unity_editor.deb -s "${URL}" 
-echo "Unity3D installer has finished downloading."
+curl -o /app/unity_editor.deb -s "${URL}"
+echo "Unpacking deb package."
+dpkg -i /app/unity_editor.deb
+echo "Remove deb package"
+rm /app/unity_editor.deb
