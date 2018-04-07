96eb80009fc367e1a7c36cc5bcbfc668b05e6bb3
Robin Fischer <t9RCtCxQZFp@gmail.com>
2018-04-07 16:46:33 +0200

    Fix url for 2017.3.1f1 version

--- unity3d/get-unity.sh
+++ unity3d/get-unity.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-URL=http://beta.unity3d.com/download/3c89f8d277f5/UnitySetup-2017.3.0f1
+URL=http://beta.unity3d.com/download/aea5ecb8f9fd/UnitySetup-2017.3.1f1
 INSTALL_LOCATION=/opt/Unity
 DOWNLOAD_LOCATION=/app/unity_download
 UNITY_COMPONENTS="Unity,Mac,Windows,Linux"
