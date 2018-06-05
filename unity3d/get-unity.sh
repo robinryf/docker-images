945ea9fedfc6f2887ee3d6a78df313a8177566da
Robin Fischer <t9RCtCxQZFp@gmail.com>
2018-06-05 17:41:10 +0200

    Update Unity version

--- unity3d/get-unity.sh
+++ unity3d/get-unity.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-URL=http://beta.unity3d.com/download/170f0691b973/UnitySetup-2018.1.0f2
+URL=http://beta.unity3d.com/download/3709a3f954c1/UnitySetup-2018.1.2f1
 INSTALL_LOCATION=/opt/Unity
 DOWNLOAD_LOCATION=/app/unity_download
 UNITY_COMPONENTS="Unity,Mac-Mono,Windows-Mono,Linux"
